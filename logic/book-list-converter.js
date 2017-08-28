a1c2e4d58e3de62814f54ee6a5d581efab4fed18
Steph Clacksman <oddsteph@gmail.com>
2017-08-28 10:15:38 +0100

    Neatened up book list converter

--- logic/book-list-converter.js
+++ logic/book-list-converter.js
@@ -4,11 +4,8 @@ const xmlParser = require("fast-xml-parser");
 
 module.exports = {
     convertToJson: (bookList) => {
-        //TODO convert from goodreads book list xml format to json
         if (xmlParser.validate(bookList)) {
-            let jsonBooks = (xmlParser.parse(bookList)).GoodreadsResponse.reviews.review;
-            console.log(jsonBooks);
-            return jsonBooks;
+            return xmlParser.parse(bookList).GoodreadsResponse.reviews.review;
         } else {
             const errorMessage = "Book list XML was invalid";
             console.error(errorMessage);
