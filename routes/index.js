1df1841ee514c4bfa66884b393628966bd196478
Steph Clacksman <oddsteph@gmail.com>
2017-09-16 10:22:18 +0100

    Added user input capability

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,28 @@
 
 const express = require("express")
     , router = express.Router()
+    , validator = require('validator')
     , goodreadsIngester = require("../logic/goodreads-ingester");
 
 router.get("/", (req, res) => {
     res.render("index", { title: "Reading Stats" });
 });
 
-router.get("/ingest/:user", (req, res) => {
-    goodreadsIngester.getBooks(req.params.user)
-        .then(books => {
-            res.status(200).send(books);
-        })
-        .catch(error => {
-            console.error(`Error ingesting book reviews: ${error}`);
-            res.status(500).send("Error ingesting book reviews");
-        });
+router.get("/ingest", (req, res) => {
+    const userId = req.query.user;
+    if (validator.isNumeric(userId)) {
+        goodreadsIngester.getBooks(req.query.user)
+            .then(books => {
+                res.status(200).send(books);
+            })
+            .catch(error => {
+                console.error(`Error ingesting book reviews: ${error}`);
+                res.status(500).send("Error ingesting book reviews");
+            });
+    } else {
+        console.error(`Error: Expected numeric user ID, received: ${userId}`);
+        res.status(500).send("User ID must be numeric");
+    }
 });
 
 module.exports = router;
